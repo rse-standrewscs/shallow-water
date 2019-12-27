90d68be677ddae8035828104f9001756a07625b2
Ferdia McKeogh <ferdia@mckeogh.tech>
2019-12-27 13:19:46 +0000

    Port and test 'spectral' module

--- common/src/constants.rs
+++ common/src/constants.rs
@@ -1,4 +1,17 @@
+use core::f64::consts::PI;
+
 pub const SINFPI3: f64 = 0.866_025_403_784_438_6;
 pub const RTF516: f64 = 0.559_016_994_374_947_5;
 pub const SINF2PI5: f64 = 0.951_056_516_295_153_5;
 pub const SINRAT: f64 = 0.618_033_988_749_894_9;
+
+pub const COF: f64 = 4.0 * PI;
+pub const CGW: f64 = 2.0 * PI;
+pub const HBAR: f64 = 0.4;
+pub const CDAMP: f64 = 10.0;
+pub const NNU: f64 = 3.0;
+
+pub const CSQ: f64 = CGW * CGW;
+pub const FSQ: f64 = COF * COF;
+pub const COFI: f64 = 1.0 / COF;
+pub const GRAVITY: f64 = CSQ / HBAR;
