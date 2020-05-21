68a49e4b83b6f2e33be042b913788ff587cc8235
Ferdia McKeogh <ferdia@mckeogh.tech>
2020-05-21 22:25:14 +0100

    Improve nhswps/cpsource parallelisation

--- src/nhswps/cpsource.rs
+++ src/nhswps/cpsource.rs
@@ -5,9 +5,7 @@ use {
         utils::{arr2zero, arr3zero},
     },
     ndarray::{ArrayViewMut3, Axis, Zip},
-    parking_lot::Mutex,
     rayon::prelude::*,
-    std::sync::Arc,
 };
 
 /// Finds the part of the pressure source which does not vary
@@ -21,21 +19,16 @@ pub fn cpsource(state: &State, mut sp0: ArrayViewMut3<f64>) {
     let mut ut = arr3zero(ng, nz);
     let mut vt = arr3zero(ng, nz);
     let mut wt = arr3zero(ng, nz);
-    let mut ux = arr2zero(ng);
-    let mut uy = arr2zero(ng);
-    let mut vx = arr2zero(ng);
-    let mut vy = arr2zero(ng);
     let mut hsrc = arr2zero(ng);
-    let mut wkp = arr2zero(ng);
-    let mut wkq = arr2zero(ng);
+    let wkq = arr2zero(ng);
 
     // Spectral space arrays (all work arrays)
     let mut wka = arr2zero(ng);
-    let mut wkb = arr2zero(ng);
+    let wkb = arr2zero(ng);
 
     // Calculate part which is independent of z, -g*Lap_h{h}:
     // wkp = h;
-    wkp.assign(&state.z.index_axis(Axis(2), nz));
+    let mut wkp = state.z.index_axis(Axis(2), nz).to_owned();
 
     // Fourier transform to spectral space:
     state.spectral.d2fft.ptospc(wkp.view_mut(), wka.view_mut());
@@ -50,234 +43,221 @@ pub fn cpsource(state: &State, mut sp0: ArrayViewMut3<f64>) {
     // Calculate u_theta, v_theta & w_theta:
 
     // Lower boundary (use higher order formula):
-    Zip::from(ut.index_axis_mut(Axis(2), 0))
-        .and(state.u.index_axis(Axis(2), 0))
-        .and(state.u.index_axis(Axis(2), 1))
-        .and(state.u.index_axis(Axis(2), 2))
-        .apply(|ut, u0, u1, u2| *ut = hdzi * (4.0 * u1 - 3.0 * u0 - u2));
+    (0..=nz as u16)
+        .into_par_iter()
+        .zip(ut.axis_iter_mut(Axis(2)).into_par_iter())
+        .zip(vt.axis_iter_mut(Axis(2)).into_par_iter())
+        .zip(wt.axis_iter_mut(Axis(2)).into_par_iter())
+        .for_each(|(((iz, ut), vt), wt)| {
+            let iz = iz as usize;
 
-    Zip::from(vt.index_axis_mut(Axis(2), 0))
-        .and(state.v.index_axis(Axis(2), 0))
-        .and(state.v.index_axis(Axis(2), 1))
-        .and(state.v.index_axis(Axis(2), 2))
-        .apply(|vt, v0, v1, v2| *vt = hdzi * (4.0 * v1 - 3.0 * v0 - v2));
+            if iz == 0 {
+                // Lower boundary (use higher order formula):
+                Zip::from(ut)
+                    .and(state.u.index_axis(Axis(2), 0))
+                    .and(state.u.index_axis(Axis(2), 1))
+                    .and(state.u.index_axis(Axis(2), 2))
+                    .apply(|ut, u0, u1, u2| *ut = hdzi * (4.0 * u1 - 3.0 * u0 - u2));
 
-    Zip::from(wt.index_axis_mut(Axis(2), 0))
-        .and(state.w.index_axis(Axis(2), 0))
-        .and(state.w.index_axis(Axis(2), 1))
-        .and(state.w.index_axis(Axis(2), 2))
-        .apply(|wt, w0, w1, w2| *wt = hdzi * (4.0 * w1 - 3.0 * w0 - w2));
+                Zip::from(vt)
+                    .and(state.v.index_axis(Axis(2), 0))
+                    .and(state.v.index_axis(Axis(2), 1))
+                    .and(state.v.index_axis(Axis(2), 2))
+                    .apply(|vt, v0, v1, v2| *vt = hdzi * (4.0 * v1 - 3.0 * v0 - v2));
 
-    // Interior (centred differencing):
-    for iz in 1..nz {
-        Zip::from(ut.index_axis_mut(Axis(2), iz))
-            .and(state.u.index_axis(Axis(2), iz + 1))
-            .and(state.u.index_axis(Axis(2), iz - 1))
-            .apply(|ut, up, um| *ut = hdzi * (up - um));
-        Zip::from(vt.index_axis_mut(Axis(2), iz))
-            .and(state.v.index_axis(Axis(2), iz + 1))
-            .and(state.v.index_axis(Axis(2), iz - 1))
-            .apply(|vt, vp, vm| *vt = hdzi * (vp - vm));
-        Zip::from(wt.index_axis_mut(Axis(2), iz))
-            .and(state.w.index_axis(Axis(2), iz + 1))
-            .and(state.w.index_axis(Axis(2), iz - 1))
-            .apply(|wt, wp, wm| *wt = hdzi * (wp - wm));
-    }
-
-    // Upper boundary (use higher order formula):
-    Zip::from(ut.index_axis_mut(Axis(2), nz))
-        .and(state.u.index_axis(Axis(2), nz))
-        .and(state.u.index_axis(Axis(2), nz - 1))
-        .and(state.u.index_axis(Axis(2), nz - 2))
-        .apply(|ut, u, u1, u2| *ut = hdzi * (3.0 * u + u2 - 4.0 * u1));
-    Zip::from(vt.index_axis_mut(Axis(2), nz))
-        .and(state.v.index_axis(Axis(2), nz))
-        .and(state.v.index_axis(Axis(2), nz - 1))
-        .and(state.v.index_axis(Axis(2), nz - 2))
-        .apply(|vt, v, v1, v2| *vt = hdzi * (3.0 * v + v2 - 4.0 * v1));
-    Zip::from(wt.index_axis_mut(Axis(2), nz))
-        .and(state.w.index_axis(Axis(2), nz))
-        .and(state.w.index_axis(Axis(2), nz - 1))
-        .and(state.w.index_axis(Axis(2), nz - 2))
-        .apply(|wt, w, w1, w2| *wt = hdzi * (3.0 * w + w2 - 4.0 * w1));
+                Zip::from(wt)
+                    .and(state.w.index_axis(Axis(2), 0))
+                    .and(state.w.index_axis(Axis(2), 1))
+                    .and(state.w.index_axis(Axis(2), 2))
+                    .apply(|wt, w0, w1, w2| *wt = hdzi * (4.0 * w1 - 3.0 * w0 - w2));
+            } else if iz == nz {
+                // Upper boundary (use higher order formula):
+                Zip::from(ut)
+                    .and(state.u.index_axis(Axis(2), nz))
+                    .and(state.u.index_axis(Axis(2), nz - 1))
+                    .and(state.u.index_axis(Axis(2), nz - 2))
+                    .apply(|ut, u, u1, u2| *ut = hdzi * (3.0 * u + u2 - 4.0 * u1));
+                Zip::from(vt)
+                    .and(state.v.index_axis(Axis(2), nz))
+                    .and(state.v.index_axis(Axis(2), nz - 1))
+                    .and(state.v.index_axis(Axis(2), nz - 2))
+                    .apply(|vt, v, v1, v2| *vt = hdzi * (3.0 * v + v2 - 4.0 * v1));
+                Zip::from(wt)
+                    .and(state.w.index_axis(Axis(2), nz))
+                    .and(state.w.index_axis(Axis(2), nz - 1))
+                    .and(state.w.index_axis(Axis(2), nz - 2))
+                    .apply(|wt, w, w1, w2| *wt = hdzi * (3.0 * w + w2 - 4.0 * w1));
+            } else {
+                // Interior (centred differencing):
+                Zip::from(ut)
+                    .and(state.u.index_axis(Axis(2), iz + 1))
+                    .and(state.u.index_axis(Axis(2), iz - 1))
+                    .apply(|ut, up, um| *ut = hdzi * (up - um));
+                Zip::from(vt)
+                    .and(state.v.index_axis(Axis(2), iz + 1))
+                    .and(state.v.index_axis(Axis(2), iz - 1))
+                    .apply(|vt, vp, vm| *vt = hdzi * (vp - vm));
+                Zip::from(wt)
+                    .and(state.w.index_axis(Axis(2), iz + 1))
+                    .and(state.w.index_axis(Axis(2), iz - 1))
+                    .apply(|wt, wp, wm| *wt = hdzi * (wp - wm));
+            }
+        });
 
     // Loop over layers and build up source, sp0:
 
-    // iz = 0 is much simpler as z = w = 0 there:
-    // Calculate u_x, u_y, v_x & v_y:
-    wkq.assign(&state.u.index_axis(Axis(2), 0));
+    (0..=nz as u16)
+        .into_par_iter()
+        .zip(sp0.axis_iter_mut(Axis(2)).into_par_iter())
+        .for_each(|(iz, mut sp0)| {
+            let iz = iz as usize;
 
-    state.spectral.d2fft.ptospc(wkq.view_mut(), wka.view_mut());
-    state
-        .spectral
-        .d2fft
-        .xderiv(&state.spectral.hrkx, wka.view(), wkb.view_mut());
-    state.spectral.d2fft.spctop(wkb.view_mut(), ux.view_mut());
-    state
-        .spectral
-        .d2fft
-        .yderiv(&state.spectral.hrky, wka.view(), wkb.view_mut());
-    state.spectral.d2fft.spctop(wkb.view_mut(), uy.view_mut());
+            let mut wka = wka.clone();
+            let mut wkb = wkb.clone();
+            let mut wkp = wkp.clone();
+            let mut wkq = wkq.clone();
+            let mut wkr = arr2zero(ng);
+            let mut ux = arr2zero(ng);
+            let mut uy = arr2zero(ng);
+            let mut vx = arr2zero(ng);
+            let mut vy = arr2zero(ng);
+            let mut wx = arr2zero(ng);
+            let mut wy = arr2zero(ng);
 
-    wkq.assign(&state.v.index_axis(Axis(2), 0));
+            // Calculate u_x, u_y, v_x & v_y:
+            wkq.assign(&state.u.index_axis(Axis(2), iz));
+            state.spectral.d2fft.ptospc(wkq.view_mut(), wka.view_mut());
+            state
+                .spectral
+                .d2fft
+                .xderiv(&state.spectral.hrkx, wka.view(), wkb.view_mut());
+            state.spectral.d2fft.spctop(wkb.view_mut(), ux.view_mut());
+            state
+                .spectral
+                .d2fft
+                .yderiv(&state.spectral.hrky, wka.view(), wkb.view_mut());
+            state.spectral.d2fft.spctop(wkb.view_mut(), uy.view_mut());
 
-    state.spectral.d2fft.ptospc(wkq.view_mut(), wka.view_mut());
-    state
-        .spectral
-        .d2fft
-        .xderiv(&state.spectral.hrkx, wka.view(), wkb.view_mut());
-    state.spectral.d2fft.spctop(wkb.view_mut(), vx.view_mut());
-    state
-        .spectral
-        .d2fft
-        .yderiv(&state.spectral.hrky, wka.view(), wkb.view_mut());
-    state.spectral.d2fft.spctop(wkb.view_mut(), vy.view_mut());
+            wkq.assign(&state.v.index_axis(Axis(2), iz));
 
-    Zip::from(&mut wkq)
-        .and(state.ri.index_axis(Axis(2), 0))
-        .and(wt.index_axis(Axis(2), 0))
-        .apply(|wkq, ri, wt| *wkq = ri * wt);
+            state.spectral.d2fft.ptospc(wkq.view_mut(), wka.view_mut());
+            state
+                .spectral
+                .d2fft
+                .xderiv(&state.spectral.hrkx, wka.view(), wkb.view_mut());
+            state.spectral.d2fft.spctop(wkb.view_mut(), vx.view_mut());
+            state
+                .spectral
+                .d2fft
+                .yderiv(&state.spectral.hrky, wka.view(), wkb.view_mut());
+            state.spectral.d2fft.spctop(wkb.view_mut(), vy.view_mut());
 
-    state.spectral.deal2d(wkq.view_mut());
+            if iz == 0 {
+                // iz = 0 is much simpler as z = w = 0 there:
 
-    Zip::from(sp0.index_axis_mut(Axis(2), 0))
-        .and(&hsrc)
-        .and(state.zeta.index_axis(Axis(2), 0))
-        .apply(|sp0, hsrc, zeta| *sp0 = hsrc + COF * zeta);
+                Zip::from(&mut wkq)
+                    .and(state.ri.index_axis(Axis(2), 0))
+                    .and(wt.index_axis(Axis(2), 0))
+                    .apply(|wkq, ri, wt| *wkq = ri * wt);
 
-    Zip::from(sp0.index_axis_mut(Axis(2), 0))
-        .and(&wkq)
-        .and(&ux)
-        .and(&uy)
-        .and(&vx)
-        .and(&vy)
-        .apply(|sp0, wkq, ux, uy, vx, vy| *sp0 += 2.0 * (ux * vy - uy * vx + wkq * (ux + vy)));
+                state.spectral.deal2d(wkq.view_mut());
 
-    let sp0 = Arc::new(Mutex::new(sp0));
+                Zip::from(&mut sp0)
+                    .and(&hsrc)
+                    .and(state.zeta.index_axis(Axis(2), 0))
+                    .apply(|sp0, hsrc, zeta| *sp0 = hsrc + COF * zeta);
 
-    (1..=nz).into_par_iter().for_each(|iz| {
-        let mut wka = wka.clone();
-        let mut wkb = wkb.clone();
-        let mut wkp = wkp.clone();
-        let mut wkq = wkq.clone();
-        let mut wkr = arr2zero(ng);
-        let mut ux = ux.clone();
-        let mut uy = uy.clone();
-        let mut vx = vx.clone();
-        let mut vy = vy.clone();
-        let mut wx = arr2zero(ng);
-        let mut wy = arr2zero(ng);
+                Zip::from(&mut sp0)
+                    .and(&wkq)
+                    .and(&ux)
+                    .and(&uy)
+                    .and(&vx)
+                    .and(&vy)
+                    .apply(|sp0, wkq, ux, uy, vx, vy| {
+                        *sp0 += 2.0 * (ux * vy - uy * vx + wkq * (ux + vy))
+                    });
+            } else {
+                // Calculate w_x, w_y:
 
-        // Calculate u_x, u_y, v_x, v_y, w_x, w_y:
-        wkq.assign(&state.u.index_axis(Axis(2), iz));
+                wkq.assign(&state.w.index_axis(Axis(2), iz));
 
-        state.spectral.d2fft.ptospc(wkq.view_mut(), wka.view_mut());
-        state
-            .spectral
-            .d2fft
-            .xderiv(&state.spectral.hrkx, wka.view(), wkb.view_mut());
-        state.spectral.d2fft.spctop(wkb.view_mut(), ux.view_mut());
-        state
-            .spectral
-            .d2fft
-            .yderiv(&state.spectral.hrky, wka.view(), wkb.view_mut());
-        state.spectral.d2fft.spctop(wkb.view_mut(), uy.view_mut());
+                state.spectral.d2fft.ptospc(wkq.view_mut(), wka.view_mut());
+                state
+                    .spectral
+                    .d2fft
+                    .xderiv(&state.spectral.hrkx, wka.view(), wkb.view_mut());
+                state.spectral.d2fft.spctop(wkb.view_mut(), wx.view_mut());
+                state
+                    .spectral
+                    .d2fft
+                    .yderiv(&state.spectral.hrky, wka.view(), wkb.view_mut());
+                state.spectral.d2fft.spctop(wkb.view_mut(), wy.view_mut());
 
-        wkq.assign(&state.v.index_axis(Axis(2), iz));
+                // Calculate pressure source:
+                Zip::from(&mut wkp)
+                    .and(vt.index_axis(Axis(2), iz))
+                    .and(ut.index_axis(Axis(2), iz))
+                    .and(state.zx.index_axis(Axis(2), iz))
+                    .and(state.zy.index_axis(Axis(2), iz))
+                    .apply(|wkp, vt, ut, zx, zy| *wkp = vt * zx - ut * zy);
 
-        state.spectral.d2fft.ptospc(wkq.view_mut(), wka.view_mut());
-        state
-            .spectral
-            .d2fft
-            .xderiv(&state.spectral.hrkx, wka.view(), wkb.view_mut());
-        state.spectral.d2fft.spctop(wkb.view_mut(), vx.view_mut());
-        state
-            .spectral
-            .d2fft
-            .yderiv(&state.spectral.hrky, wka.view(), wkb.view_mut());
-        state.spectral.d2fft.spctop(wkb.view_mut(), vy.view_mut());
+                state.spectral.deal2d(wkp.view_mut());
 
-        wkq.assign(&state.w.index_axis(Axis(2), iz));
+                Zip::from(&mut wkq)
+                    .and(&uy)
+                    .and(vt.index_axis(Axis(2), iz))
+                    .and(ut.index_axis(Axis(2), iz))
+                    .and(&vy)
+                    .apply(|wkq, uy, vt, ut, vy| *wkq = uy * vt - ut * vy);
 
-        state.spectral.d2fft.ptospc(wkq.view_mut(), wka.view_mut());
-        state
-            .spectral
-            .d2fft
-            .xderiv(&state.spectral.hrkx, wka.view(), wkb.view_mut());
-        state.spectral.d2fft.spctop(wkb.view_mut(), wx.view_mut());
-        state
-            .spectral
-            .d2fft
-            .yderiv(&state.spectral.hrky, wka.view(), wkb.view_mut());
-        state.spectral.d2fft.spctop(wkb.view_mut(), wy.view_mut());
+                state.spectral.deal2d(wkq.view_mut());
 
-        // Calculate pressure source:
-        Zip::from(&mut wkp)
-            .and(vt.index_axis(Axis(2), iz))
-            .and(ut.index_axis(Axis(2), iz))
-            .and(state.zx.index_axis(Axis(2), iz))
-            .and(state.zy.index_axis(Axis(2), iz))
-            .apply(|wkp, vt, ut, zx, zy| *wkp = vt * zx - ut * zy);
+                Zip::from(&mut wkr)
+                    .and(ut.index_axis(Axis(2), iz))
+                    .and(vt.index_axis(Axis(2), iz))
+                    .and(&vx)
+                    .and(&ux)
+                    .apply(|wkr, ut, vt, vx, ux| *wkr = ut * vx - ux * vt);
 
-        state.spectral.deal2d(wkp.view_mut());
+                state.spectral.deal2d(wkr.view_mut());
 
-        Zip::from(&mut wkq)
-            .and(&uy)
-            .and(vt.index_axis(Axis(2), iz))
-            .and(ut.index_axis(Axis(2), iz))
-            .and(&vy)
-            .apply(|wkq, uy, vt, ut, vy| *wkq = uy * vt - ut * vy);
+                Zip::from(&mut wkq)
+                    .and(&wkr)
+                    .and(state.zx.index_axis(Axis(2), iz))
+                    .and(state.zy.index_axis(Axis(2), iz))
+                    .and(&wy)
+                    .and(vt.index_axis(Axis(2), iz))
+                    .apply(|wkq, wkr, zx, zy, wy, vt| *wkq = *wkq * zx + wkr * zy - wy * vt);
 
-        state.spectral.deal2d(wkq.view_mut());
+                Zip::from(&mut wkq)
+                    .and(&ux)
+                    .and(&vy)
+                    .and(ut.index_axis(Axis(2), iz))
+                    .and(wt.index_axis(Axis(2), iz))
+                    .and(&wx)
+                    .apply(|wkq, ux, vy, ut, wt, wx| *wkq += (ux + vy) * wt - wx * ut);
 
-        Zip::from(&mut wkr)
-            .and(ut.index_axis(Axis(2), iz))
-            .and(vt.index_axis(Axis(2), iz))
-            .and(&vx)
-            .and(&ux)
-            .apply(|wkr, ut, vt, vx, ux| *wkr = ut * vx - ux * vt);
+                state.spectral.deal2d(wkq.view_mut());
 
-        state.spectral.deal2d(wkr.view_mut());
+                //sp0(:,:,iz)=hsrc+cof*(zeta(:,:,iz)-ri(:,:,iz)*wkp)+ two*(ux*vy-uy*vx+ri(:,:,iz)*wkq);
+                Zip::from(&mut sp0)
+                    .and(&hsrc)
+                    .and(state.zeta.index_axis(Axis(2), iz))
+                    .and(state.ri.index_axis(Axis(2), iz))
+                    .and(&wkp)
+                    .and(&wkq)
+                    .apply(|sp0, hsrc, zeta, ri, wkp, wkq| {
+                        *sp0 = hsrc + COF * (zeta - ri * wkp) + 2.0 * (ri * wkq)
+                    });
 
-        Zip::from(&mut wkq)
-            .and(&wkr)
-            .and(state.zx.index_axis(Axis(2), iz))
-            .and(state.zy.index_axis(Axis(2), iz))
-            .and(&wy)
-            .and(vt.index_axis(Axis(2), iz))
-            .apply(|wkq, wkr, zx, zy, wy, vt| *wkq = *wkq * zx + wkr * zy - wy * vt);
-
-        Zip::from(&mut wkq)
-            .and(&ux)
-            .and(&vy)
-            .and(ut.index_axis(Axis(2), iz))
-            .and(wt.index_axis(Axis(2), iz))
-            .and(&wx)
-            .apply(|wkq, ux, vy, ut, wt, wx| *wkq += (ux + vy) * wt - wx * ut);
-
-        state.spectral.deal2d(wkq.view_mut());
-        //sp0(:,:,iz)=hsrc+cof*(zeta(:,:,iz)-ri(:,:,iz)*wkp)+ two*(ux*vy-uy*vx+ri(:,:,iz)*wkq);
-        let mut temp = arr2zero(ng);
-
-        Zip::from(&mut temp)
-            .and(&hsrc)
-            .and(state.zeta.index_axis(Axis(2), iz))
-            .and(state.ri.index_axis(Axis(2), iz))
-            .and(&wkp)
-            .and(&wkq)
-            .apply(|t, hsrc, zeta, ri, wkp, wkq| {
-                *t = hsrc + COF * (zeta - ri * wkp) + 2.0 * (ri * wkq)
-            });
-
-        Zip::from(&mut temp)
-            .and(&ux)
-            .and(&vy)
-            .and(&uy)
-            .and(&vx)
-            .apply(|t, ux, vy, uy, vx| *t += 2.0 * (ux * vy - uy * vx));
-
-        sp0.lock().index_axis_mut(Axis(2), iz).assign(&temp);
-    });
+                Zip::from(&mut sp0)
+                    .and(&ux)
+                    .and(&vy)
+                    .and(&uy)
+                    .and(&vx)
+                    .apply(|sp0, ux, vy, uy, vx| *sp0 += 2.0 * (ux * vy - uy * vx));
+            }
+        });
 }
 
 #[cfg(test)]
